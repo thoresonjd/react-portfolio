1c8a1cd095aee9ecb08d4e8269a55d788454fb7a
thoresonjd <thoresonjd@gmail.com>
2022-08-14 20:45:33 -0700

    Route to home page configured

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <>Hello, world!</>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
