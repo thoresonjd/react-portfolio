bfe983c2d1c08d686b5d03124a31182c30e645e7
thoresonjd <thoresonjd@gmail.com>
2022-08-18 15:01:32 -0700

    Switched from BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material';
 import { 
   Navbar,
