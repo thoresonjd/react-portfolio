08cdfff1e5f40f1c57da7b8e4cf3d7899a28d6e6
thoresonjd <thoresonjd@gmail.com>
2022-09-19 12:36:27 -0700

    Vignette

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Typed from 'react-typed';
-import { Avatar, Typography, Slide } from '@mui/material';
+import { Avatar, Typography, Zoom } from '@mui/material';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useHomeStyles } from './home-styles';
 import avatar from '../../assets/images/alien.jpg';
@@ -12,13 +12,13 @@ export const Home: React.FC = () => {
     <Container className={classes.container}>
       <Row className={classes.row}>
         <Col className={classes.image} md={6}>
-          <Slide direction='down' in={true} mountOnEnter unmountOnExit>
+          <Zoom in={true} mountOnEnter unmountOnExit>
             <Avatar
               src={avatar}
               alt='avatar'
               className={classes.avatar}
             />
-          </Slide>
+          </Zoom>
         </Col>
         <Col className={classes.typedBox} md={6} >
           <Typography variant='h3' className={classes.title}>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { AppBar, Toolbar } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useNavbarStyles } from './navbar-styles';
