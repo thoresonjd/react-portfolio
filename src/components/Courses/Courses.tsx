c0651058ab9e5a649816c657c330ae6d13471c43
thoresonjd <thoresonjd@gmail.com>
2022-12-19 21:09:52 -0800

    Created data for courses in db

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,58 +1,7 @@
 import React from 'react';
 import { Table, TableContainer, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
 import { Paper } from '@mui/material';
-
-const data = [
-  {
-    number: 'CPSC 100',
-    name: 'Computing 1',
-    quarter: 'Fall',
-    year: '2022',
-    institution: 'Seattle University'
-  },
-  {
-    number: 'CPSC 100',
-    name: 'Computing 1',
-    quarter: 'Fall',
-    year: '2022',
-    institution: 'Seattle University'
-  },
-  {
-    number: 'CPSC 100',
-    name: 'Computing 1',
-    quarter: 'Fall',
-    year: '2022',
-    institution: 'Seattle University'
-  },
-  {
-    number: 'CPSC 100',
-    name: 'Computing 1',
-    quarter: 'Fall',
-    year: '2022',
-    institution: 'Seattle University'
-  },
-  {
-    number: 'CPSC 100',
-    name: 'Computing 1',
-    quarter: 'Fall',
-    year: '2022',
-    institution: 'Seattle University'
-  },
-  {
-    number: 'CPSC 100',
-    name: 'Computing 1',
-    quarter: 'Fall',
-    year: '2022',
-    institution: 'Seattle University'
-  },
-  {
-    number: 'CPSC 100',
-    name: 'Computing 1',
-    quarter: 'Fall',
-    year: '2022',
-    institution: 'Seattle University'
-  },
-]
+import courses from '../../db/courses.json';
 
 export const Courses: React.FC = () => {
   return (
@@ -60,21 +9,23 @@ export const Courses: React.FC = () => {
       <Table>
         <TableHead>
           <TableRow>
+            <TableCell>Institution</TableCell>
             <TableCell>Number</TableCell>
             <TableCell>Name</TableCell>
             <TableCell>Quarter</TableCell>
             <TableCell>Year</TableCell>
-            <TableCell>Institution</TableCell>
+            <TableCell>Grade</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row) => (
-            <TableRow>
-              <TableCell>{row.number}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.quarter}</TableCell>
-              <TableCell>{row.year}</TableCell>
-              <TableCell>{row.institution}</TableCell>
+          {courses.map((course: any, cIndex: number) => (
+            <TableRow key={cIndex}>
+              <TableCell>{course.institution}</TableCell>
+              <TableCell>{course.number}</TableCell>
+              <TableCell>{course.name}</TableCell>
+              <TableCell>{course.quarter}</TableCell>
+              <TableCell>{course.year}</TableCell>
+              <TableCell>{course.grade}</TableCell>
             </TableRow>
           ))}
         </TableBody>
