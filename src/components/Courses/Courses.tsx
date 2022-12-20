3a114df4c857bd80e8858c0b5557e44852effdef
thoresonjd <thoresonjd@gmail.com>
2022-12-19 20:01:17 -0800

    Created courses table structure

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,7 +1,84 @@
 import React from 'react';
+import { Table, TableContainer, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { Paper } from '@mui/material';
+
+const data = [
+  {
+    number: 'CPSC 100',
+    name: 'Computing 1',
+    quarter: 'Fall',
+    year: '2022',
+    institution: 'Seattle University'
+  },
+  {
+    number: 'CPSC 100',
+    name: 'Computing 1',
+    quarter: 'Fall',
+    year: '2022',
+    institution: 'Seattle University'
+  },
+  {
+    number: 'CPSC 100',
+    name: 'Computing 1',
+    quarter: 'Fall',
+    year: '2022',
+    institution: 'Seattle University'
+  },
+  {
+    number: 'CPSC 100',
+    name: 'Computing 1',
+    quarter: 'Fall',
+    year: '2022',
+    institution: 'Seattle University'
+  },
+  {
+    number: 'CPSC 100',
+    name: 'Computing 1',
+    quarter: 'Fall',
+    year: '2022',
+    institution: 'Seattle University'
+  },
+  {
+    number: 'CPSC 100',
+    name: 'Computing 1',
+    quarter: 'Fall',
+    year: '2022',
+    institution: 'Seattle University'
+  },
+  {
+    number: 'CPSC 100',
+    name: 'Computing 1',
+    quarter: 'Fall',
+    year: '2022',
+    institution: 'Seattle University'
+  },
+]
 
 export const Courses: React.FC = () => {
   return (
-    <></>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Number</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Quarter</TableCell>
+            <TableCell>Year</TableCell>
+            <TableCell>Institution</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow>
+              <TableCell>{row.number}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.quarter}</TableCell>
+              <TableCell>{row.year}</TableCell>
+              <TableCell>{row.institution}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
