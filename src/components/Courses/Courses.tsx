5869b56f0152bf3e96946d968bdf683dc6a42440
thoresonjd <thoresonjd@gmail.com>
2022-12-21 19:01:06 -0800

    Added title to courses page

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -1,39 +1,49 @@
 import React from 'react';
-import { Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Fade } from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+import { Table, TableContainer, TableBody, TableCell, TableHead, TableRow, Fade, Typography } from '@mui/material';
 import courses from '../../db/courses.json';
 import { useStyles } from '../../theme';
 
-const columns: string[] = ["Institution", "Number", "Name", "Quarter", "Year", "Grade"];
+const columns: string[] = ['Institution', 'Number', 'Name', 'Quarter', 'Year', 'Grade'];
 
 export const Courses: React.FC = () => {
+  const title = 'Courses';
   const classes: any = useStyles();
   const transitionDelay: number = 25;
 
   return (
-    <TableContainer className={classes.courseContainer}>
-      <Table className={classes.courseTable}>
-        <TableHead>
-          <TableRow>
-            {columns.map((col: string, colIndex: number) => (
-              <TableCell className={classes.itemHeader} key={colIndex}>{col}</TableCell>
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <Typography className={classes.timelineTitle} variant='h4'>
+        {title}
+      </Typography>
+      <TableContainer className={classes.courseContainer}>
+        <Table className={classes.courseTable}>
+          <TableHead>
+            <TableRow>
+              {columns.map((col: string, colIndex: number) => (
+                <TableCell className={classes.itemHeader} key={colIndex}>{col}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {courses.map((course: any, courseIndex: number) => (
+              <Fade key={courseIndex} in={true} style={{ transitionDelay: `${transitionDelay * courseIndex}ms` }}>
+                <TableRow>
+                  <TableCell className={classes.bodyText}>{course.institution}</TableCell>
+                  <TableCell className={classes.bodyText}>{course.number}</TableCell>
+                  <TableCell className={classes.bodyText}>{course.name}</TableCell>
+                  <TableCell className={classes.bodyText}>{course.quarter}</TableCell>
+                  <TableCell className={classes.bodyText}>{course.year}</TableCell>
+                  <TableCell className={classes.bodyText}>{course.grade}</TableCell>
+                </TableRow>
+              </Fade>
             ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {courses.map((course: any, courseIndex: number) => (
-            <Fade key={courseIndex} in={true} style={{ transitionDelay: `${transitionDelay * courseIndex}ms` }}>
-              <TableRow>
-                <TableCell className={classes.bodyText}>{course.institution}</TableCell>
-                <TableCell className={classes.bodyText}>{course.number}</TableCell>
-                <TableCell className={classes.bodyText}>{course.name}</TableCell>
-                <TableCell className={classes.bodyText}>{course.quarter}</TableCell>
-                <TableCell className={classes.bodyText}>{course.year}</TableCell>
-                <TableCell className={classes.bodyText}>{course.grade}</TableCell>
-              </TableRow>
-            </Fade>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
