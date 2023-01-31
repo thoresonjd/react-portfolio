1ddbb38270cf147e330ca0feff39ae1e0d028da8
thoresonjd <thoresonjd@gmail.com>
2023-01-30 18:42:23 -0800

    Removed unused import

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, Typography, Zoom } from '@mui/material';
+import { Card, CardContent, Zoom } from '@mui/material';
 import { 
   Timeline as TimelineMUI, 
   TimelineItem,
