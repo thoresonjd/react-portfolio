b313b0c2fd557c4df6d2dc4d6db0fe02e42aa110
thoresonjd <thoresonjd@gmail.com>
2023-05-14 13:31:35 -0700

    Modified about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Typography, Avatar, List, ListItem, Icon, Link, Zoom, Fade } from '@mui/material';
 import { useStyles } from '../../theme';
+import avatar from '../../assets/images/alien-lime.png';
+import name from '../../assets/gifs/justin-jitter.gif';
+import alias from '../../assets/gifs/exulgor-jitter.gif';
 import about from '../../db/about.json';
 
 export const About: React.FC = (): JSX.Element => {
@@ -16,18 +19,29 @@ export const About: React.FC = (): JSX.Element => {
       <div className={classes.aboutBox}>
         <Zoom in={true}>
           <Avatar
-            src={require(`../../${about['avatar-path']}`)}
-            alt='avatar'
             className={classes.avatar}
+            src={avatar}
+            alt='avatar'
           />
         </Zoom>
         <Zoom in={true}>
           <img
             className={classes.aboutName}
             alt='name'
-            src={require(`../../${about['name-gif-path']}`)}
+            src={name}
           />
         </Zoom>
+        <div className={classes.bodyText}>a.k.a</div>      
+        <Zoom in={true}>
+          <img
+            className={classes.aboutName}
+            alt='alias'
+            src={alias}
+          />
+        </Zoom>
+        <div className={classes.aboutDescriptionBox}>
+          <div className={classes.bodyText}>{about.description}</div>
+        </div>      
         <div className={classes.linksBox}>
           <List className={classes.linksList}>
             {about.links.map((link: any, lIndex: number) => (
